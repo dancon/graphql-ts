56b86602141b507a36a1c24aa3b14d85d88454ac
dancon <870301137@qq.com>
2020-01-15 10:48:39 +0800

    feat: start rewrite language/ast part

---
+++ src/language/ast/interface.ts
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview
+ * @author john.hou | 870301137@qq.com
+ * @version 1.0.0 | 2020-01-15 | john.hou      // initial version
+ */
+
+export interface DocumentNode {
+  readonly kink: 'Document'
+  readonly loc?: Location
+}
--- src/language/parser/index.ts
+++ src/language/parser/index.ts
@@ -4,4 +4,21 @@
  * @version 1.0.0 | 2020-01-13 | john.hou      // initial version
  */
 
+import { ParseOption } from './interface'
+import { Source } from '../source'
+
+/**
+ * 用来将 source 转化为 GraphQL Document
+ * @param source
+ * @param options
+ */
+export function parse(source: string | Source, options?: ParseOption) {
+  const parser = new Parser()
+  return parser.parseDocument()
+}
+
+class Parser {
+  parseDocument () {}
+}
+
 export * from './interface'
